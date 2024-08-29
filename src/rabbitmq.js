4df3942e29b10d3288dad27f956531dd934b002d
notariq <hauzanbakri@example.com>
2024-08-29 15:02:50 +0700

    fix rabbit

--- src/rabbitmq.js
+++ src/rabbitmq.js
@@ -1,7 +1,10 @@
 const amqp = require('amqplib/callback_api');
+const Playlist = require('./models/playlist');
 
 let channel = null;
 const MQ_URI = process.env.MQ_URI || 'amqp://localhost:5672';
+
+const RABBIT_MQ_QUEUES = ['NEW_MUSIC', 'DELETE_MUSIC', 'DELETE_USER']
 
 // Connect to RabbitMQ
 amqp.connect(MQ_URI, (error0, connection) => {
@@ -14,16 +17,56 @@ amqp.connect(MQ_URI, (error0, connection) => {
         }
         console.log('Connected to RabbitMQ');
         channel = ch;
+        
+        // Initiates Queues
+        RABBIT_MQ_QUEUES.map((queue) => {
+            channel.assertQueue(queue, { durable: true });
+            channel.consume(queue, (msg) => {
+                if (msg !== null) {
+                    channel.ack(msg);
+                    console.log('Message consumed from', queue);
+                    //queueController(queue, msg)
+                }
+            });
+        });
     });
 });
 
-// Function to publish a message to a queue
+function queueController(queue, msg) {
+    switch(queue) {
+        case 'DELETE_USER': 
+            userDelete(msg);
+            break
+        case 'DELTE_MUSIC':
+            musicDelete(msg)
+            break
+        default:
+            console.log(`No handler found for queue: ${queue}`);
+    }
+};
+
+const userDelete = async (msg) => {
+    const username = JSON.parse(msg.content.toString())
+    try {
+        const result = await Playlist.deleteMany({ createdBy: username });
+        if (result.deletedCount === 0) {
+            console.log('No playlists found for the given user id');
+        } else {
+            console.log(`${result.deletedCount} playlist(s) deleted successfully`);
+        }
+    } catch (error) {
+        console.error('Error deleting playlists:', error);
+    }
+    
+};
+
 const publishToQueue = async (queueName, data) => {
     if (!channel) {
         throw new Error("Channel is not created yet");
     }
     channel.assertQueue(queueName, { durable: true });
     channel.sendToQueue(queueName, Buffer.from(JSON.stringify(data)), { persistent: true });
+    console.log('Message created for', queueName);
 };
 
 // Function to consume messages from a queue
@@ -40,34 +83,4 @@ const consumeQueue = async (queueName, callback) => {
     });
 };
 
-// Function to subscribe to an exchange
-const subscribeToExchange = async (exchangeName, queueName, routingKey, callback) => {
-    if (!channel) {
-        throw new Error("Channel is not created yet");
-    }
-
-    // Declare an exchange
-    channel.assertExchange(exchangeName, 'fanout', { durable: false });
-
-    // Declare a queue
-    channel.assertQueue(queueName, { exclusive: true }, (error, q) => {
-        if (error) {
-            throw error;
-        }
-        
-        // Bind the queue to the exchange
-        channel.bindQueue(q.queue, exchangeName, routingKey);
-        
-        // Start consuming messages from the queue
-        channel.consume(q.queue, (msg) => {
-            if (msg !== null) {
-                callback(JSON.parse(msg.content.toString()));
-                channel.ack(msg);
-            }
-        }, { noAck: false });
-
-        console.log(`Subscribed to exchange '${exchangeName}' with queue '${q.queue}' and routing key '${routingKey}'`);
-    });
-};
-
-module.exports = { publishToQueue, consumeQueue, subscribeToExchange };
+module.exports = { publishToQueue, consumeQueue };
